5f7414df404a1f5466df8bfba4517130ed15ac27
panirwalak <panirwalak@Khadijas-MacBook-Pro.local>
2018-08-04 12:46:51 -0400

    parallelized bitonic sort code

--- bitonic2.jl
+++ bitonic2.jl
@@ -1,9 +1,9 @@
 @everywhere function kernel(a, p, q)
     d = 1 << (p-q)
     #dist = fld(length(a),2)
-        @sync @parallel for i=0:(length(a)-1)
+    for i=0:(length(a)-1)
         up = ((i>>p) & 2) == 0
-        # println(((i & d) == 0 && ((a[i+1] > a[(i | d)+1]))))
+        #println(((i & d) == 0 && ((a[i+1] > a[(i | d)+1]))))
         if((i & d) == 0 && ((a[i+1] > a[(i | d)+1]) == up))
             t = a[i+1]
             a[i+1] = a[(i | d)+1]
@@ -13,10 +13,25 @@
 end
 
 
-@everywhere function bitonicSort(logn, a)
-    for i=0:(logn-1)
+
+function bitonicSort(logn, a)
+    a=SharedArray(a)
+    @sync @parallel for i=0:(logn-1)
         for j=0:i
             kernel(a, i, j)
         end
     end
+    # return a
+
 end
+
+a = Array{Int64}(16777216)
+
+rng = MersenneTwister(1234);
+
+SIZE = 16777216
+
+arr = rand(rng, SIZE)
+# for i=1:16777216 a[i] = i%5 end
+
+@time bitonicSort(24,a)
